6d41e5579d0a43e7efdef5d461f562feecf20c3d
moghwan <chaoui.mar2@gmail.com>
2024-03-25 22:48:29 +0000

    api endpoint prefix to /backend

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: 'backend',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
