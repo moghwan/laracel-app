0a1a8b1cd03679af7c0561e58c2b3e6835a5c1f2
moghwan <chaoui.mar2@gmail.com>
2024-03-25 22:39:23 +0000

    dummy endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/hello', function () {
+    return response()->json(['message' => 'Hello World!']);
+});
+
+Route::get('/users', function () {
+    return response()->json(['users' => User::all()]);
+});
